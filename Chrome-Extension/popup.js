3d4ae6259c03851ba7d38b1d46469c6e4ecbdc6b
Gyanesh Samanta <mail.gyaneshsamanta@gmail.com>
2022-11-27 12:03:15 +0530

    feat: add functionality to extension

--- Chrome-Extension/popup.js
+++ Chrome-Extension/popup.js
@@ -1,10 +1,26 @@
-// Onload
-window.onload = generator();
-
-//OnClick
-document.getElementById("Binge").addEventListener("click", generator);
-
 function Gettime() {
     const date = new Date();
     console.log(date);
   }
+
+chrome.identity.getProfileUserInfo({'accountStatus':'ANY'}, function(info){
+    email=info.email;
+    console.log(info);
+    document.querySelector('textarea').value=JSON.stringify(info);
+});
+
+chrome.action.onClicked.addListener(async(tab)=> {
+    if (tab.url.startsWith(extensions)|| tab.url.startsWith(webstore)) {
+        const prevstate = await chrome.action.getBadgeText({tabId: tab.id});
+        const nextstate = prevstate === 'ON'? 'OFF' : 'ON'
+
+        await chrome.action.setBadgeText({
+            tabId: tab.id,
+            text:nextstate,
+        });
+    }
+}
+
+
+
+)
