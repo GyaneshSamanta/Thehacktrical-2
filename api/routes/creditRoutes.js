9c3119c3ed54a51b6a43a7b709f950e34adc2237
Eshaan-B <eshaan.bhardy@gmail.com>
2022-11-27 16:20:26 +0530

    added endpoints

--- api/routes/creditRoutes.js
+++ api/routes/creditRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const sdk = require("node-appwrite");
+const { listeners } = require("../models/user");
 
 const client = new sdk.Client();
 const databases = new sdk.Databases(client, "63830306843fefca4d57");
@@ -12,16 +13,78 @@ client
     "db65f0c11f3960efeb9bd9ad0e5a8515c3d0c17a8967cd66b63fd0c518b6d7ddfb84d9785a73949db58a52878a7de4fb772c63ad13b0bab5b3c19bc11017e538e1f90acb992723ee2dd0844b3dc8b07bef23b5ea9ecf76bc0b92b461bee6a984e218509bc51e31e06d69acf71d5dcf6b3c3897d5c99fe62e21c03b38ced4b66c"
   );
 
+const fetching = databases.listDocuments(
+  "63830306843fefca4d57",
+  "6383032c362272b9688c"
+);
+
 router.get("/fetchAll", (req, res, next) => {
-  databases
-    .listDocuments("63830306843fefca4d57", "6383032c362272b9688c")
-    .then((response) => {
-      console.log("All documents are: ", response);
-      res.send(response);
-    })
-    .catch((err) => {
-      console.log("Error: ", err);
-    });
+  fetching.then((response) => {
+    console.log("All documents are: ", response);
+    res.send(response);
+  });
+});
+
+router.post("/start", (req, res, next) => {
+  var status;
+  fetching.then((result) => {
+    const docs = result.documents;
+    var doc = docs.filter((doc) => doc.email === req.body.email);
+    doc = doc[0];
+    res.send(doc.status);
+  });
+});
+
+router.get("/stop", (req, res, next) => {
+  const time = req.body.time;
+  var docId;
+  var credits;
+  fetching.then((result) => {
+    const docs = result.documents;
+    var doc = docs.filter((doc) => doc.email == "8hardy10@gmail.com");
+    doc = doc[0];
+    docId = doc.$id;
+    credits = doc.credits - 0.001 * 6000;
+    console.log(credits);
+    databases
+      .updateDocument("63830306843fefca4d57", "6383032c362272b9688c", docId, {
+        credits: parseInt(credits),
+      })
+      .then((result) => {
+        console.log("Doc updated");
+        res.send(`Updated credits: ${credits}`);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.send(err);
+      });
+  });
+});
+
+router.get("/score", (req, res, next) => {
+  const score = req.body.score;
+  console.log("Score is: ", score);
+
+  fetching.then((result) => {
+    const docs = result.documents;
+    var doc = docs.filter((doc) => doc.email == "8hardy10@gmail.com");
+    doc = doc[0];
+    docId = doc.$id;
+    credits = doc.credits + 10;
+    console.log(credits);
+    databases
+      .updateDocument("63830306843fefca4d57", "6383032c362272b9688c", docId, {
+        credits: parseInt(credits),
+      })
+      .then((result) => {
+        console.log("Score increased");
+        res.send(`Updated credits: ${credits}`);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.send(err);
+      });
+  });
 });
 
 module.exports = router;
