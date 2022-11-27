b34b16784c2ab7f463acd7f8c4a4a5fcba59442a
Sudhanshu Srivastava <ss1163@armist.edu.in>
2022-11-27 16:48:34 +0530

    Connecting extension to backend

--- api/routes/creditRoutes.js
+++ api/routes/creditRoutes.js
@@ -7,7 +7,7 @@ const client = new sdk.Client();
 const databases = new sdk.Databases(client, "63830306843fefca4d57");
 client
   .setSelfSigned(true)
-  .setEndpoint("https://localhost/v1") // Your API Endpoint
+  .setEndpoint("https://192.168.235.84/v1") // Your API Endpoint
   .setProject("638301ff5e5a0c2339f2") // Your project ID
   .setKey(
     "db65f0c11f3960efeb9bd9ad0e5a8515c3d0c17a8967cd66b63fd0c518b6d7ddfb84d9785a73949db58a52878a7de4fb772c63ad13b0bab5b3c19bc11017e538e1f90acb992723ee2dd0844b3dc8b07bef23b5ea9ecf76bc0b92b461bee6a984e218509bc51e31e06d69acf71d5dcf6b3c3897d5c99fe62e21c03b38ced4b66c"
--- app.js
+++ app.js
@@ -9,14 +9,14 @@ const morgan = require("morgan");
 const authRoutes = require("./api/routes/authRoutes");
 const creditRoutes = require("./api/routes/creditRoutes");
 app.use(express.static(path.join(__dirname, "public")));
-
+const cors = require('cors')
 const sdk = require("node-appwrite");
 
 const client = new sdk.Client();
 const databases = new sdk.Databases(client, "63830306843fefca4d57");
 client
   .setSelfSigned(true)
-  .setEndpoint("https://localhost/v1") // Your API Endpoint
+  .setEndpoint("https://192.168.235.84/v1") // Your API Endpoint
   .setProject("638301ff5e5a0c2339f2") // Your project ID
   .setKey(
     "db65f0c11f3960efeb9bd9ad0e5a8515c3d0c17a8967cd66b63fd0c518b6d7ddfb84d9785a73949db58a52878a7de4fb772c63ad13b0bab5b3c19bc11017e538e1f90acb992723ee2dd0844b3dc8b07bef23b5ea9ecf76bc0b92b461bee6a984e218509bc51e31e06d69acf71d5dcf6b3c3897d5c99fe62e21c03b38ced4b66c"
@@ -26,7 +26,7 @@ client
 // appwrite
 //     .setEndpoint('http://localhost/v1')
 //     .setProject('638301ff5e5a0c2339f2');
-
+app.use(cors())
 app.set("view engine", "ejs");
 app.set("views", "views");
 
